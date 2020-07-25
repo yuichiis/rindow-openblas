3a5a312d66e24a829496a8418d0b97f953f067d6
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-07-25 18:38:57 +0900

    update Math.c

--- src/Rindow/OpenBLAS/Math.c
+++ src/Rindow/OpenBLAS/Math.c
@@ -1803,319 +1803,6 @@ static int rindow_openblas_math_set_float(
     }
 }
 
-/*
-   Y(i) := A(X[i])
-
-   Method Rindow\OpenBLAS\Math::
-    public function selectAxis0(
-        int $m,
-        int $n,
-        int $k,
-        Buffer $A, int $offsetA, int $ldA,
-        Buffer $X, int $offsetX, int $incX,
-        Buffer $Y, int $offsetY, int $ldY ) : void
- {{{ */
-static PHP_METHOD(Math, selectAxis0)
-{
-    php_rindow_openblas_buffer_t* bufferA;
-    php_rindow_openblas_buffer_t* bufferX;
-    php_rindow_openblas_buffer_t* bufferY;
-    zend_long m;
-    zend_long n;
-    zend_long k;
-    zval* a=NULL;
-    zend_long offsetA;
-    zend_long ldA;
-    zval* x=NULL;
-    zend_long offsetX;
-    zend_long incX;
-    zval* y=NULL;
-    zend_long offsetY;
-    zend_long ldY;
-
-    ZEND_PARSE_PARAMETERS_START_EX(ZEND_PARSE_PARAMS_THROW, 12, 12)
-        Z_PARAM_LONG(m)
-        Z_PARAM_LONG(n)
-        Z_PARAM_LONG(k)
-        Z_PARAM_OBJECT_OF_CLASS(a,php_rindow_openblas_buffer_ce)
-        Z_PARAM_LONG(offsetA)
-        Z_PARAM_LONG(ldA)
-        Z_PARAM_OBJECT_OF_CLASS(x,php_rindow_openblas_buffer_ce)
-        Z_PARAM_LONG(offsetX)
-        Z_PARAM_LONG(incX)
-        Z_PARAM_OBJECT_OF_CLASS(y,php_rindow_openblas_buffer_ce)
-        Z_PARAM_LONG(offsetY)
-        Z_PARAM_LONG(ldY)
-    ZEND_PARSE_PARAMETERS_END();
-
-    if(php_rindow_openblas_assert_shape_parameter(
-        PHP_RINDOW_OPENBLAS_ASSERT_M, m)) {
-        return;
-    }
-    if(php_rindow_openblas_assert_shape_parameter(
-        PHP_RINDOW_OPENBLAS_ASSERT_N, n)) {
-        return;
-    }
-    if(php_rindow_openblas_assert_shape_parameter(
-        PHP_RINDOW_OPENBLAS_ASSERT_K, k)) {
-        return;
-    }
-    // Check Buffer A
-    bufferA = Z_RINDOW_OPENBLAS_BUFFER_OBJ_P(a);
-    if(php_rindow_openblas_assert_matrix_buffer_spec(
-        PHP_RINDOW_OPENBLAS_ASSERT_A, bufferA,m,n,offsetA,ldA)) {
-        return;
-    }
-
-    // Check Buffer X
-    bufferX = Z_RINDOW_OPENBLAS_BUFFER_OBJ_P(x);
-    if(php_rindow_openblas_assert_vector_buffer_spec(
-        PHP_RINDOW_OPENBLAS_ASSERT_X, bufferX,k,offsetX,incX)) {
-        return;
-    }
-
-    // Check Buffer Y
-    bufferY = Z_RINDOW_OPENBLAS_BUFFER_OBJ_P(y);
-    if(php_rindow_openblas_assert_matrix_buffer_spec(
-        PHP_RINDOW_OPENBLAS_ASSERT_Y, bufferY,k,n,offsetY,ldY)) {
-        return;
-    }
-
-    // Check Buffer A and Y
-    if(bufferA->dtype!=bufferY->dtype) {
-        zend_throw_exception(spl_ce_InvalidArgumentException, "Unmatch data type for A and Y", 0);
-        return;
-    }
-    if(bufferX->dtype==php_rindow_openblas_dtype_bool) {
-        zend_throw_exception(spl_ce_InvalidArgumentException, "Data type of BufferX must not be bool", 0);
-        return;
-    }
-
-    switch (bufferA->dtype) {
-        case php_rindow_openblas_dtype_float32:
-            {
-                float *a = &(((float *)bufferA->data)[offsetA]);
-                float *y = &(((float *)bufferY->data)[offsetY]);
-                zend_long i,selector;
-                for(i=0; i<k; i++,y+=ldY) {
-                    if(rindow_openblas_math_get_integer(
-                                bufferX->dtype, bufferX->data, offsetX,incX,
-                                i, &selector)) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Unsupported data type of label number.", 0);
-                        return;
-                    }
-                    if(selector<0||selector>=m) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Label number is out of bounds.", 0);
-                        return;
-                    }
-                    if(n==1) {
-                        *y = a[selector*ldA];
-                    } else {
-                        cblas_scopy((blasint)n,
-                            &(a[selector*ldA]), (blasint)1,
-                            y, (blasint)1);
-                    }
-                }
-            }
-            break;
-        case php_rindow_openblas_dtype_float64:
-            {
-                double *a = &(((double *)bufferA->data)[offsetA]);
-                double *y = &(((double *)bufferY->data)[offsetX]);
-                zend_long i,selector;
-                for(i=0; i<k; i++,y+=ldY) {
-                    if(rindow_openblas_math_get_integer(
-                                bufferX->dtype, bufferX->data, offsetX,incX,
-                                i, &selector)) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Unsupported data type of label number.", 0);
-                        return;
-                    }
-                    if(selector<0||selector>=m) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Label number is out of bounds.", 0);
-                        return;
-                    }
-                    if(n==1) {
-                        *y = a[selector*ldA];
-                    } else {
-                        cblas_dcopy((blasint)n,
-                            &(a[selector*ldA]), (blasint)1,
-                            y, (blasint)1);
-                    }
-                }
-            }
-            break;
-        default:
-            if(!php_rindow_openblas_dtype_is_int(bufferA->dtype)&&
-                !php_rindow_openblas_dtype_is_bool(bufferA->dtype)) {
-                zend_throw_exception(spl_ce_RuntimeException, "Unsupported data type.", 0);
-                return;
-            }
-            {
-                int valueSize;
-                uint8_t *a, *y;
-                valueSize = php_rindow_openblas_dtype_to_valuesize(bufferA->dtype);
-
-                a = php_rindow_openblas_get_address(bufferA,offsetA,valueSize);
-                y = php_rindow_openblas_get_address(bufferY,offsetY,valueSize);
-                zend_long i,selector,incrementY,selectSizeA,copySize;
-                incrementY = ldY*valueSize;
-                selectSizeA = ldA*valueSize;
-                copySize = n*valueSize;
-                for(i=0; i<k; i++,y+=incrementY) {
-                    if(rindow_openblas_math_get_integer(
-                                bufferX->dtype, bufferX->data, offsetX,incX,
-                                i, &selector)) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Unsupported data type of label number.", 0);
-                        return;
-                    }
-                    if(selector<0||selector>=m) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Label number is out of bounds.", 0);
-                        return;
-                    }
-                    memcpy(y, &(a[selector*selectSizeA]), copySize);
-                }
-            }
-            break;
-    }
-}
-
-/*
-   Y(i) := A(X[i])
-
-   Method Rindow\OpenBLAS\Math::
-    public function selectAxis1(
-        int $m,
-        int $n,
-        Buffer $A, int $offsetA, int $ldA,
-        Buffer $X, int $offsetX, int $incX,
-        Buffer $Y, int $offsetY, int $incY ) : void
- {{{ */
-static PHP_METHOD(Math, selectAxis1)
-{
-    php_rindow_openblas_buffer_t* bufferA;
-    php_rindow_openblas_buffer_t* bufferX;
-    php_rindow_openblas_buffer_t* bufferY;
-    zend_long m;
-    zend_long n;
-    zval* a=NULL;
-    zend_long offsetA;
-    zend_long ldA;
-    zval* x=NULL;
-    zend_long offsetX;
-    zend_long incX;
-    zval* y=NULL;
-    zend_long offsetY;
-    zend_long incY;
-
-    //if (zend_parse_parameters(ZEND_NUM_ARGS(), "llOllOllOll",
-    //        &m,
-    //        &n,
-    //        &a,php_rindow_openblas_buffer_ce,&offsetA,&ldA,
-    //        &x,php_rindow_openblas_buffer_ce,&offsetX,&incX,
-    //        &y,php_rindow_openblas_buffer_ce,&offsetY,&incY) == FAILURE) {
-    //    zend_throw_exception(spl_ce_InvalidArgumentException, "Invalid Arguments", 0);
-    //    return;
-    //}
-    ZEND_PARSE_PARAMETERS_START_EX(ZEND_PARSE_PARAMS_THROW, 11, 11)
-        Z_PARAM_LONG(m)
-        Z_PARAM_LONG(n)
-        Z_PARAM_OBJECT_OF_CLASS(a,php_rindow_openblas_buffer_ce)
-        Z_PARAM_LONG(offsetA)
-        Z_PARAM_LONG(ldA)
-        Z_PARAM_OBJECT_OF_CLASS(x,php_rindow_openblas_buffer_ce)
-        Z_PARAM_LONG(offsetX)
-        Z_PARAM_LONG(incX)
-        Z_PARAM_OBJECT_OF_CLASS(y,php_rindow_openblas_buffer_ce)
-        Z_PARAM_LONG(offsetY)
-        Z_PARAM_LONG(incY)
-    ZEND_PARSE_PARAMETERS_END();
-
-    if(php_rindow_openblas_assert_shape_parameter(
-        PHP_RINDOW_OPENBLAS_ASSERT_M, m)) {
-        return;
-    }
-    if(php_rindow_openblas_assert_shape_parameter(
-        PHP_RINDOW_OPENBLAS_ASSERT_N, n)) {
-        return;
-    }
-    // Check Buffer A
-    bufferA = Z_RINDOW_OPENBLAS_BUFFER_OBJ_P(a);
-    if(php_rindow_openblas_assert_matrix_buffer_spec(
-        PHP_RINDOW_OPENBLAS_ASSERT_A, bufferA,m,n,offsetA,ldA)) {
-        return;
-    }
-
-    // Check Buffer X
-    bufferX = Z_RINDOW_OPENBLAS_BUFFER_OBJ_P(x);
-    if(php_rindow_openblas_assert_vector_buffer_spec(
-        PHP_RINDOW_OPENBLAS_ASSERT_X, bufferX,m,offsetX,incX)) {
-        return;
-    }
-
-    // Check Buffer Y
-    bufferY = Z_RINDOW_OPENBLAS_BUFFER_OBJ_P(y);
-    if(php_rindow_openblas_assert_vector_buffer_spec(
-        PHP_RINDOW_OPENBLAS_ASSERT_Y, bufferY,m,offsetY,incY)) {
-        return;
-    }
-
-    // Check Buffer A and Y
-    if(bufferA->dtype!=bufferY->dtype) {
-        zend_throw_exception(spl_ce_InvalidArgumentException, "Unmatch data type for A and Y", 0);
-        return;
-    }
-    if(bufferX->dtype==php_rindow_openblas_dtype_bool) {
-        zend_throw_exception(spl_ce_InvalidArgumentException, "Data type of BufferX must not be bool", 0);
-        return;
-    }
-
-    switch (bufferA->dtype) {
-        case php_rindow_openblas_dtype_float32:
-            {
-                float *a = &(((float *)bufferA->data)[offsetA]);
-                float *y = &(((float *)bufferY->data)[offsetY]);
-                zend_long i,selector;
-                for(i=0; i<m; i++) {
-                    if(rindow_openblas_math_get_integer(
-                                bufferX->dtype, bufferX->data, offsetX,incX,
-                                i, &selector)) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Unsupported data type of label number.", 0);
-                        return;
-                    }
-                    if(selector<0||selector>=n) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Label number is out of bounds.", 0);
-                        return;
-                    }
-                    y[i*incY] = a[i*ldA+selector];
-                }
-            }
-            break;
-        case php_rindow_openblas_dtype_float64:
-            {
-                double *a = &(((double *)bufferA->data)[offsetA]);
-                double *y = &(((double *)bufferY->data)[offsetX]);
-                zend_long i,selector;
-                for(i=0; i<m; i++) {
-                    if(rindow_openblas_math_get_integer(
-                                bufferX->dtype, bufferX->data, offsetX,incX,
-                                i, &selector)) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Unsupported data type of label number.", 0);
-                        return;
-                    }
-                    if(selector<0||selector>=n) {
-                        zend_throw_exception(spl_ce_RuntimeException, "Label number is out of bounds.", 0);
-                        return;
-                    }
-                    y[i*incY] = a[i*ldA+selector];
-                }
-            }
-            break;
-        default:
-            zend_throw_exception(spl_ce_RuntimeException, "Unsupported data type.", 0);
-            return;
-    }
-}
-/* }}} */
 
 /*
    Y := a * onehot(X) + Y
@@ -2872,6 +2559,9 @@ static PHP_METHOD(Math, astype)
 }
 /* }}} */
 
+
+#include "Math_select.c"
+#include "Math_scatter.c"
 #include "Math_im2col1d.c"
 #include "Math_im2col2d.c"
 #include "Math_im2col3d.c"
@@ -3065,6 +2755,35 @@ ZEND_BEGIN_ARG_INFO_EX(ai_Math_selectAxis1, 0, 0, 11)
     ZEND_ARG_INFO(0, incY)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(ai_Math_scatterAxis0, 0, 0, 12)
+    ZEND_ARG_INFO(0, m)
+    ZEND_ARG_INFO(0, n)
+    ZEND_ARG_INFO(0, k)
+    ZEND_ARG_OBJ_INFO(0, a, Rindow\\OpenBLAS\\Buffer, 0)
+    ZEND_ARG_INFO(0, offsetA)
+    ZEND_ARG_INFO(0, ldA)
+    ZEND_ARG_OBJ_INFO(0, x, Rindow\\OpenBLAS\\Buffer, 0)
+    ZEND_ARG_INFO(0, offsetX)
+    ZEND_ARG_INFO(0, incX)
+    ZEND_ARG_OBJ_INFO(0, y, Rindow\\OpenBLAS\\Buffer, 0)
+    ZEND_ARG_INFO(0, offsetY)
+    ZEND_ARG_INFO(0, ldY)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(ai_Math_scatterAxis1, 0, 0, 11)
+    ZEND_ARG_INFO(0, m)
+    ZEND_ARG_INFO(0, n)
+    ZEND_ARG_OBJ_INFO(0, a, Rindow\\OpenBLAS\\Buffer, 0)
+    ZEND_ARG_INFO(0, offsetA)
+    ZEND_ARG_INFO(0, ldA)
+    ZEND_ARG_OBJ_INFO(0, x, Rindow\\OpenBLAS\\Buffer, 0)
+    ZEND_ARG_INFO(0, offsetX)
+    ZEND_ARG_INFO(0, incX)
+    ZEND_ARG_OBJ_INFO(0, y, Rindow\\OpenBLAS\\Buffer, 0)
+    ZEND_ARG_INFO(0, offsetY)
+    ZEND_ARG_INFO(0, incY)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(ai_Math_updateAddOnehot, 0, 0, 9)
     ZEND_ARG_INFO(0, m)
     ZEND_ARG_INFO(0, n)
@@ -3238,6 +2957,8 @@ static zend_function_entry php_rindow_openblas_math_me[] = {
     PHP_ME(Math, zeros,          ai_Math_zeros,          ZEND_ACC_PUBLIC)
     PHP_ME(Math, selectAxis0,    ai_Math_selectAxis0,    ZEND_ACC_PUBLIC)
     PHP_ME(Math, selectAxis1,    ai_Math_selectAxis1,    ZEND_ACC_PUBLIC)
+    PHP_ME(Math, scatterAxis0,    ai_Math_scatterAxis0,    ZEND_ACC_PUBLIC)
+    PHP_ME(Math, scatterAxis1,    ai_Math_scatterAxis1,    ZEND_ACC_PUBLIC)
     PHP_ME(Math, updateAddOnehot,ai_Math_updateAddOnehot,ZEND_ACC_PUBLIC)
     PHP_ME(Math, softmax,        ai_Math_softmax,        ZEND_ACC_PUBLIC)
     PHP_ME(Math, equal,          ai_Math_equal,          ZEND_ACC_PUBLIC)
