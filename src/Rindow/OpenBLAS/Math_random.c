85e30356918c72b5f20e9ebbfff53053c6956c87
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-02 07:50:53 +0900

    update Math_random.c

--- src/Rindow/OpenBLAS/Math_random.c
+++ src/Rindow/OpenBLAS/Math_random.c
@@ -71,10 +71,10 @@ static PHP_METHOD(Math, randomUniform)
                 float *x = &(((float *)bufferX->data)[offsetX]);
                 zend_long i;
                 for(i=0; i<n; i++,x+=incX) {
-                    *x = (float)PHP_MT_RAND_MAX;
-                    /*
-                    *x = (float)low_float+(float)php_mt_rand()*((float)high_float-(float)low_float+1.0)/(1.0+(float)PHP_MT_RAND_MAX);
-                    */
+                    //*x = (float)PHP_MT_RAND_MAX;
+                    
+                    *x = (float)low_float+(float)php_mt_rand()/(float)PHP_MT_RAND_MAX*((float)high_float-(float)low_float);
+                    
                 }
             }
             break;
